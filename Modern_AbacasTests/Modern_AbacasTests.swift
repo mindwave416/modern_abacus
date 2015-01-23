0d1c0b9402ba6b48acb968e3337e99593834e759
mindwave416 <bnaugle87@gmail.com>
2015-01-22 22:17:12 -0700

    Initial Commit

---
+++ Modern_AbacasTests/Modern_AbacasTests.swift
@@ -0,0 +1,36 @@
+//
+//  Modern_AbacasTests.swift
+//  Modern_AbacasTests
+//
+//  Created by Benjamin Naugle on 1/22/15.
+//  Copyright (c) 2015 0806564. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Modern_AbacasTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
